589f585ed646624cc67d5e44cbde268467b49369
merg-hz <shirou-z@outlook.jp>
2023-04-30 02:41:19 +0900

    Update

--- Home.py
+++ Home.py
@@ -4,15 +4,20 @@ import tkinter as tk
 import requests
 from bs4 import BeautifulSoup as bsp4
 import re
-from Play import Play
+from Play import Play_w
 from function import ScrollableFrame
 
 song_list = []
 btn_list = []
-def Open_Song(s_url = "", title=""):
-    Play(s_url, title)
+def Open_Song(i):
+    def x():
+        global song_list
+        play = Play_w(song_list[i][0], song_list[i][1])
+    return x
 
 def Search(btn_list, frame):
+    global song_list
+    song_list = []
     for i in range(len(btn_list)):
         btn_list[i].destroy()
     tt = title_text.get()
@@ -25,11 +30,12 @@ def Search(btn_list, frame):
     for i in range(len(elements)):
         k_u = str(elements[i].a.get('href'))
         t_t = str(elements[i].a.text).replace(" ", "").replace('\n', '')
+        song_list.append([k_u, t_t])
         btn = ttk.Button(frame.scrollable_frame, 
                          text= t_t + str(elements2[i].a.text).replace(" ", ""),
                          padding=(5,10),
                          width=60,
-                         command=lambda:Open_Song(k_u, t_t))
+                         command=Open_Song(i))
         btn.pack(side = "top")
         btn_list.append(btn)
 root = Tk()
--- Play.py
+++ Play.py
@@ -4,69 +4,118 @@ import tkinter as t
 import requests
 from bs4 import BeautifulSoup as bsp4
 import re
+import regex
+import pykakasi
 
-start_flag = False
-ctn = 3
-label = None
-entry = None
-root = None
-kasi_list = None
-def Play(url, title):
-    global kasi_list
-    res = requests.get("https://utaten.com" + url)
-    soup = bsp4(res.text, "html.parser")
-    elem = str(soup.find("div", class_="hiragana"))
-    print(elem)
-    exit()
-    kasi_list = elem.split('<br/>')
-    kasi_list = [s for s in kasi_list if s]
+class Play_w:
+    def __init__(self, url, title):
+        self.start_flag = False
+        self.ctn = 3
+        self.label = None
+        self.label2 = None
+        self.label3 = None
+        self.root = None
+        self.kasi_list = []
+        self.now_str = None
 
-    global label
-    global root
-    root = Tk()
-    root.title(title)
-    root.geometry("600x300")
-   
-    label = ttk.Label(root, text="スペースキーでスタート", font=70)
-    label.pack()
-   
-    root.bind("<KeyPress>", Key_down)
-    root.mainloop()
+        kakasi = pykakasi.kakasi()
+        kakasi.setMode('H', 'a')
+        kakasi.setMode('K', 'a')
+        self.conversion = kakasi.getConverter()
 
-def Key_down(e):
-    global key
-    global start_flag
-    key = e.keycode
-    if key == 32 and start_flag == False:
-        start_flag = True
-        count()
-    elif key == 13 and start_flag == True:
-        global kasi_list, entry
-        check_text = str(entry.get()).replace(" ","")
-        if check_text.casefold() == kasi_list[0].casefold():
-            entry.delete(0, t.END)
-            kasi_list.pop(0)
-            Show()
-
-def count():
-    global label
-    global ctn
-    label["text"] = ctn
-    ctn -= 1
-    if ctn == -1:
-        Show()
-    else:
-        root.after(1000, count)
-
-def Show():
-    global kasi_list, entry, root
-    if entry == None:
-        entry = ttk.Entry(root, width=300)
-        entry.pack()
+        res = requests.get("https://utaten.com" + url)
+        soup = bsp4(res.text, "html.parser")
+        elem = str(soup.find("div", class_="hiragana"))[22:-7].strip()
+        elem = elem.replace('<span class="ruby"><span class="rb">', ",").replace("</span></span>", ",").replace('</span><span class="rt">', ",").replace("\n", "").replace("<br>", ",<br>,").replace("<br/>", ",<br>,")
+        elems = elem.split(",")
+        elems = [s for s in elems if s]
+        kari = ["",""]
+        p = re.compile('[\u0000-\u007F]+')
+        kan = regex.compile(r'\p{Script=Han}+')
+        i = 0
+        while i < len(elems):
+            if i != 0:
+                if elems[i] == "<br>" and elems[i - 1] == "<br>":
+                    i += 1
+                    continue
     
-    if len(kasi_list) == 0:
-        label["text"] = "終了"
-    else:
-        entry.focus_set()
-        label["text"] = kasi_list[0]
-        kasi_list[0].replace(" ", "")
+            if elems[i] == "<br>":
+                self.kasi_list.append(kari)
+                kari = ["", ""]
+            elif p.fullmatch(elems[i]):
+                kari[0] += elems[i]
+                kari[1] += elems[i]
+            elif kan.fullmatch(elems[i]):
+                kari[0] += elems[i]
+                kari[1] += elems[i + 1]
+                i += 1
+            else:
+                kari[0] += elems[i]
+                kari[1] += elems[i]
+            i += 1
+        self.root = Tk()
+        self.root.title(title)
+        self.root.geometry("1000x400")
+        self.root.minsize(width=800, height=400)
+       
+        self.label = ttk.Label(self.root, text="スペースキーでスタート", font=("", 30))
+        self.label2 = ttk.Label(self.root, font=("", 25))
+        self.label3 = ttk.Label(self.root, text="\n", font=("", 20))
+    
+        self.label.pack()
+        self.label2.pack()
+        self.label3.pack()
+       
+        self.root.bind("<KeyPress>", self.Key_down)
+        
+        self.root.mainloop()
+
+    def Key_down(self, e):
+        key = e.keysym
+        if key == "space" and self.start_flag == False:
+            self.start_flag = True
+            self.count()
+            return
+        
+        if key == "comma":
+            key = ","
+        elif key == "exclam":
+            key = "!"
+        elif key == "period":
+            key = "."
+        elif key == "question":
+            key = "?"
+        elif key =="minus":
+            key = "-"
+        p = re.compile(r'a|i|u|e|o')
+        if self.now_str[0:2] == "ch" and key == "t" or self.now_str[0:2] == "sh" and key == "s" or self.now_str[0:2] == "ts" and key == "t":
+            self.label3['text'] += key
+            self.now_str =self.now_str[2:]
+        elif  self.now_str[0] == key or self.now_str[0] == "j" and key == "z" or self.now_str[0] == "f" and key == "h" or self.now_str[0] == "z" and key == "d":
+            if len(self.now_str) == 1:
+                self.label3['text'] = "\n"
+                self.kasi_list.pop(0)
+                self.Show()
+                return
+            self.label3['text'] += key
+            self.now_str = self.now_str[1:]
+        elif self.now_str[0] == "h" and key == "y" and self.label3['text'][-1] == "s" or p.search(self.now_str[0]) and self.label3['text'][-2:] == "ch":
+            self.label3['text'] += key
+            self.now_str = self.now_str[1:]
+    
+    def count(self):
+        self.label["text"] = self.ctn
+        self.ctn -= 1
+        if self.ctn == -1:
+            self.Show()
+        else:
+            self.root.after(1000, self.count)
+    
+    def Show(self):        
+        if len(self.kasi_list) == 0:
+            self.label["text"] = "終了"
+            self.label2["text"] = ""
+        else:
+            self.label["text"] = self.kasi_list[0][0]
+            self.label2["text"] = self.kasi_list[0][1]
+            self.now_str = self.conversion.do(self.kasi_list[0][1].replace("\n", " ").replace("\r", " ").replace(" ", "").replace("　", "").replace("「","").replace("」","").replace("、", ",").lower())

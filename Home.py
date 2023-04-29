db0aa46274977e7ec25b77d7d0f4520545308d9e
merg-hz <shirou-z@outlook.jp>
2023-04-29 22:40:33 +0900

    Update

--- Home.py
+++ Home.py
@@ -1,2 +1,53 @@
-print("こんにちは")
-input("なんか入力して")
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
+import requests
+from bs4 import BeautifulSoup as bsp4
+import re
+from Play import Play
+from function import ScrollableFrame
+
+song_list = []
+btn_list = []
+def Open_Song(s_url = "", title=""):
+    Play(s_url, title)
+
+def Search(btn_list, frame):
+    for i in range(len(btn_list)):
+        btn_list[i].destroy()
+    tt = title_text.get()
+    url = 'https://search2.j-lyric.net/index.php?ex=on&ct=2&ca=2&cl=2&kt='+ tt +'&search=検索'
+    res = requests.get(url)
+    soup = bsp4(res.text, "html.parser")
+    elements = soup.find_all("div", class_="bdy")
+    for i in range(len(elements)):
+        element = elements[i].find("a" ,href=re.compile("j-lyric.net/artist/"))
+        if element == None:
+            continue
+        k_u = str(element.get('href'))
+        t_t = str(element.text)
+        btn = ttk.Button(frame.scrollable_frame, 
+                         text= element.text + ' : ' + str(element.get('title'))[0:-(len(str(element.text)) + 4)],
+                         padding=(5,10),
+                         width=60,
+                         command=lambda:Open_Song(k_u, t_t))
+        btn.pack(side = "top")
+        btn_list.append(btn)
+root = Tk()
+root.title("Main Window")
+root.geometry("800x500")
+root.minsize(width=400, height=400)
+
+frame = ScrollableFrame(root)
+
+label = ttk.Label(root, text = "曲名を入力", font=("", 20), padding=(5, 20), compound=CENTER)
+title_text = StringVar()
+entry = ttk.Entry(root, width=60, textvariable=title_text)
+button = ttk.Button(root, text = "OK", command=lambda:Search(btn_list, frame))
+
+label.pack()
+entry.pack()
+button.pack()
+frame.pack()
+
+root.mainloop()
---
+++ Play.py
@@ -0,0 +1,70 @@
+from tkinter import *
+from tkinter import ttk
+import tkinter as t
+import requests
+from bs4 import BeautifulSoup as bsp4
+import re
+
+start_flag = False
+ctn = 3
+label = None
+entry = None
+root = None
+kasi_list = None
+def Play(url, title):
+    global kasi_list
+    res = requests.get(url)
+    soup = bsp4(res.text, "html.parser")
+    elem = str(soup.find("p", id="Lyric"))[14:-30]
+    kasi_list = elem.split('<br/>')
+    kasi_list = [s for s in kasi_list if s]
+
+    global label
+    global root
+    root = Tk()
+    root.title(title)
+    root.geometry("600x300")
+   
+    label = ttk.Label(root, text="スペースキーでスタート", font=70)
+    label.pack()
+   
+    root.bind("<KeyPress>", Key_down)
+    root.mainloop()
+
+def Key_down(e):
+    global key
+    global start_flag
+    key = e.keycode
+    if key == 32 and start_flag == False:
+        start_flag = True
+        count()
+    elif key == 13 and start_flag == True:
+        global kasi_list, entry
+        check_text = str(entry.get()).replace(" ","")
+        if check_text.casefold() == kasi_list[0].casefold():
+            entry.delete(0, t.END)
+            kasi_list.pop(0)
+            Show()
+
+def count():
+    global label
+    global ctn
+    label["text"] = ctn
+    ctn -= 1
+    if ctn == -1:
+        Show()
+    else:
+        root.after(1000, count)
+
+def Show():
+    global kasi_list, entry, root
+    if entry == None:
+        entry = ttk.Entry(root, width=300)
+        entry.pack()
+    
+    if len(kasi_list) == 0:
+        label["text"] = "終了"
+    else:
+        entry.focus_set()
+        label["text"] = kasi_list[0]
+        kasi_list[0].replace(" ", "")
